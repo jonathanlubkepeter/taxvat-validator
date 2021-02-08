682730d46165ea8062b8b5a1a619e78e7ad922c2
jonathan peter <jonathan_lpeter@yahoo.com.br>
2021-02-08 13:56:50 -0300

    Upgrade Unit tests

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # taxvat-validator
 Does not allow creating new users with the same "taxvat" number and allows to login by "taxvat" number.
 
+# Important
+Leave taxvat configured as required in client settings
+
+Admin -> Stores -> Configuration -> Customers -> Customer Configuration -> Name And Address Options -> Show Tax/Vat Number -> Required
+
 # Vendor name
 PeterTecnology
---
+++ Test/Unit/Plugin/Customer/Model/AccountManagementPluginTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Compasso\TaxvatValidator\Test\Unit\Plugin\Customer\Model;
+
+use Magento\Customer\Model\Customer;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Magento\Customer\Model\AccountManagement;
+use Compasso\TaxvatValidator\Plugin\Customer\Model\AccountManagementPlugin;
+use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
+
+class AccountManagementPluginTest extends TestCase
+{
+    /**
+     * @var MockObject
+     */
+    protected $customersMock;
+
+    protected $customerMock;
+
+    protected $accountManagementMock;
+
+    protected $accountManagementPluginMock;
+
+    protected $collectionFactory;
+
+    protected function setUp()
+    {
+        $this->accountManagementMock = $this->createMock(AccountManagement::class);
+
+        $this->customerMock = $this->getMockBuilder(Customer::class)
+            ->setMethods(['getTaxvat'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->customerMock->method('getTaxvat')->willReturn('02546445030');
+
+
+        $this->customersMock = $this->getMockBuilder(Customer::class)
+            ->setMethods(['getTaxvat'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+
+
+        $this->collectionFactory = $this->createMock(CollectionFactory::class);
+        $this->collectionFactory->method('create')->willReturn([$this->customersMock]);
+
+        $this->accountManagementPluginMock = $this->getMockBuilder(AccountManagementPlugin::class)
+            ->setMethods(['__construct'])
+            ->setConstructorArgs(
+                [
+                    $this->collectionFactory
+                ]
+            )
+            ->getMock();
+    }
+
+    /**
+     * @test
+     */
+    public function testReturnArrayToCreateAccountTaxvatIsNotExistInCollection()
+    {
+        $result = $this->accountManagementPluginMock->beforeCreateAccount($this->accountManagementMock, $this->customerMock, null);
+        $this->customersMock->method('getTaxvat')->willReturn(null);
+        $this->assertEquals([$this->customerMock, null, ''], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function testException()
+    {
+        $this->customersMock->method('getTaxvat')->willReturn('02546445030');
+        $this->expectException(\Exception::class);
+        $this->accountManagementPluginMock->beforeCreateAccount($this->accountManagementMock, $this->customerMock, null);
+
+    }
+
+}
