e3dac432bca213777055a246e788c15ee09bfc8e
jonathan peter <jonathan_lpeter@yahoo.com.br>
2021-07-30 13:53:38 -0300

    atualizacao

--- Plugin/Customer/Model/AccountManagementPlugin.php
+++ Plugin/Customer/Model/AccountManagementPlugin.php
@@ -1,37 +1,82 @@
 <?php
 
-namespace PeterTecnology\TaxvatValidator\Plugin\Customer\Model;
+namespace PeterTechnology\TaxvatValidator\Plugin\Customer\Model;
 
-use Exception;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Model\AccountManagement;
-use Magento\Customer\Model\CustomerFactory;
-use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\FilterGroupBuilder;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Exception\InputException;
+use Psr\Log\LoggerInterface as Logger;
 
 class AccountManagementPlugin
 {
-    protected $customerFactory;
-    protected $collectionFactory;
+    /**
+     * @var Logger
+     */
+    protected $logger;
 
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $customerRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    protected $searchCriteriaBuilder;
+
+    /**
+     * @var FilterGroupBuilder
+     */
+    protected $filterGroupBuilder;
+
+    /**
+     * @var FilterBuilder
+     */
+    protected $filterBuilder;
+
+
+    /**
+     * @param Logger $logger
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param FilterGroupBuilder $filterGroupBuilder
+     * @param FilterBuilder $filterBuilder
+     */
     public function __construct(
-        CustomerFactory $customerFactory,
-        CollectionFactory $collectionFactory
+        Logger $logger,
+        CustomerRepositoryInterface $customerRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterGroupBuilder $filterGroupBuilder,
+        FilterBuilder $filterBuilder
     ) {
-        $this->customerFactory = $customerFactory;
-        $this->collectionFactory = $collectionFactory;
+        $this->logger = $logger;
+        $this->customerRepository = $customerRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterGroupBuilder = $filterGroupBuilder;
+        $this->filterBuilder = $filterBuilder;
     }
 
+    /**
+     * @param AccountManagement $subject
+     * @param $customer
+     * @param null $password
+     * @param string $redirectUrl
+     * @return array
+     * @throws InputException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function beforeCreateAccount(
         AccountManagement $subject,
         $customer,
         $password = null,
         $redirectUrl = ''
-    ) {
-        $taxvat = $customer->getTaxvat();
-
-        try {
-            $this->taxvatExist($taxvat);
-        } catch (InputException $e) {
+    )
+    {
+        if ($this->taxvatExist($customer->getTaxvat()) != 0) {
+            $this->logger->error("CPF já existe: " . $customer->getTaxvat());
             throw new InputException(
                 __('Um cliente já foi associado à esse CPF.')
             );
@@ -40,51 +85,72 @@ class AccountManagementPlugin
         return [$customer, $password, $redirectUrl];
     }
 
+    /**
+     * @param AccountManagement $subject
+     * @param $username
+     * @param $password
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function beforeAuthenticate(
         AccountManagement $subject,
         $username,
         $password
-    ) {
+    )
+    {
         if (strpos($username, '@') === false) {
-
-            try {
-                $customerEmail = $this->returnEmail($username);
-            } catch (InputException $e) {
-                throw new InputException(
-                    __('We did not find any email associated with this cpf.')
-                );
-            }
-
-            $username = $customerEmail;
-
-            return [$username, $password];
+            $username = $this->getEmailByTaxvat($username);
         }
+
+        return [$username, $password];
     }
 
+    /**
+     * @param String $taxvat
+     * @return int|void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     protected function taxvatExist(String $taxvat)
     {
-        $customers = $this->customerFactory->create()
-            ->getCollection()
-            ->addAttributeToSelect('taxvat')
-            ->addAttributeToFilter('taxvat', array('eq' => $taxvat))
-            ->load();
+        $customer = $this->filterBuilder->setField('taxvat')
+            ->setValue($taxvat)
+            ->setConditionType('eq')
+            ->create();
 
-        if (count($customers) != 0) {
-            throw new InputException(
-                __('Um cliente já foi associado à esse CPF.')
-            );
-        }
-        return false;
+        $filter = $this->filterGroupBuilder
+            ->addFilter($customer)
+            ->create();
+
+        $this->searchCriteriaBuilder->setFilterGroups([$filter]);
+
+        $qtd = count($this->customerRepository->getList($this->searchCriteriaBuilder->create())->getItems());
+
+        return $qtd;
     }
 
-    protected function returnEmail(String $taxvat)
+    /**
+     * @param String $taxvat
+     * @return string|void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function getEmailByTaxvat(String $taxvat)
     {
-        $customers = $this->collectionFactory->create();
+        $customer = $this->filterBuilder->setField('taxvat')
+            ->setValue($taxvat)
+            ->setConditionType('eq')
+            ->create();
 
-        foreach ($customers as $key => $cust) {
-            if ($cust->getTaxvat() === $taxvat) {
-                return $cust->getEmail();
-            }
+        $filter = $this->filterGroupBuilder
+            ->addFilter($customer)
+            ->create();
+
+        $this->searchCriteriaBuilder->setFilterGroups([$filter]);
+
+        $customerData = $this->customerRepository->getList($this->searchCriteriaBuilder->create())->getItems();
+
+        foreach ($customerData as $cust) {
+            return $cust->getEmail();
         }
+        $this->logger->error("Nenhum email encontrado para o CPF: $taxvat");
     }
 }
--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'PeterTecnology_TaxvatValidator', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'PeterTechnology_TaxvatValidator', __DIR__);
